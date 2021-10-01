4d3a613c841bb725c6b9212b69b8c77555ad34a1
Praveen <dpraveen453@gmail.com>
2021-10-01 18:01:44 +0530

    commit changes

---
+++ src/Upload_Clip.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Input, Menu, Dropdown, message, Checkbox, Divider, Upload, Button , Tag } from 'antd';
+import { DownOutlined, UploadOutlined } from '@ant-design/icons';
+import EditableTagGroup from './Tag'
+
+const onClick = ({ key }) => {
+    message.info(`Click on item ${key}`);
+};
+
+const menu = (
+    <Menu onClick={onClick}>
+        <Menu.Item key="1">1st menu item</Menu.Item>
+        <Menu.Item key="2">2nd menu item</Menu.Item>
+        <Menu.Item key="3">3rd menu item</Menu.Item>
+    </Menu>
+);
+
+const CheckboxGroup = Checkbox.Group;
+
+const plainOptions = ['Apple', 'Pear', 'Orange'];
+const defaultCheckedList = ['Apple', 'Orange'];
+
+
+const props = {
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange({ file, fileList }) {
+      if (file.status !== 'uploading') {
+        console.log(file, fileList);
+      }
+    },
+    defaultFileList: [
+      {
+        uid: '1',
+        name: 'xxx.png',
+        status: 'done',
+        response: 'Server Error 500', // custom error message to show
+        url: 'http://www.baidu.com/xxx.png',
+      },
+      {
+        uid: '2',
+        name: 'yyy.png',
+        status: 'done',
+        url: 'http://www.baidu.com/yyy.png',
+      },
+      {
+        uid: '3',
+        name: 'zzz.png',
+        status: 'error',
+        response: 'Server Error 500', // custom error message to show
+        url: 'http://www.baidu.com/zzz.png',
+      },
+    ],
+  };
+
+
+function Upload_Clip() {
+    const [checkedList, setCheckedList] = React.useState(defaultCheckedList);
+    const [indeterminate, setIndeterminate] = React.useState(true);
+    const [checkAll, setCheckAll] = React.useState(false);
+
+    const onChange = list => {
+        setCheckedList(list);
+        setIndeterminate(!!list.length && list.length < plainOptions.length);
+        setCheckAll(list.length === plainOptions.length);
+    };
+
+    const onCheckAllChange = e => {
+        setCheckedList(e.target.checked ? plainOptions : []);
+        setIndeterminate(false);
+        setCheckAll(e.target.checked);
+    };
+
+    return (
+        <div>
+            <p>Hiii</p>
+            <p>Enter Title</p>
+            <Input placeholder="Basic usage" size="large" maxLength="140" />
+            <p>Channel Name</p>
+            <Dropdown overlay={menu}>
+                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                    Hover me, Click menu item <DownOutlined />
+                </a>
+            </Dropdown>
+            <p>Language</p>
+            <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
+                Check all
+            </Checkbox>
+            <Divider />
+            <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} />
+            <p>upload file</p>
+            <Upload {...props}>
+                <Button icon={<UploadOutlined />}>Upload</Button>
+            </Upload>
+            <EditableTagGroup/>
+            <Button>Upload Clip</Button>
+
+        </div>
+    );
+}
+
+export default Upload_Clip;
